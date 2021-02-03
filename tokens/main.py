99be7efbb2471c82eded3e20bd8ab9ba39005a1d
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2021-02-03 13:00:27 +1100

    Server (#6)
    * Add server component of the analysis runner.
    
    * Fix JWT decode.
    
    * Motivation for aiohttp.
    
    * Driver instructions.
    
    * wip
    
    * wip
    
    * Use per-project secrets.
    
    * Update lint configs.
    
    * Fix conda Python version.
    
    * Sync dependencies.
    
    * Fix conda path.
    
    * Default shell.
    
    * Add pylint-quotes.
    
    * Comment out pre-commit pylint for now.
    
    * wip
    
    * wip
    
    * More linter updates.
    
    * Only keep lint.yaml workflow.
    
    * Actually use lint.yaml.
    
    * metadata
    
    * server image
    
    * Additional env.
    
    * Fix secret name.
    
    * Fix _read_secret.
    
    * base64-decode the Hail token secret.
    
    * Fix secret decoding.
    
    * Fix clone command.
    
    * Activate gcloud.
    
    * Fix metadata path.
    
    * Update readme.
    
    * Change working directory before running the script.
    
    * Add dataset to metadata.
    
    * More parameter sanity checks.
    
    * Fix parameters.
    
    * Update server/README.md
    
    Co-authored-by: Vlad Savelyev <vladislav.sav@gmail.com>
    
    * Update server/README.md
    
    Co-authored-by: Vlad Savelyev <vladislav.sav@gmail.com>
    
    Co-authored-by: Vlad Savelyev <vladislav.sav@gmail.com>

--- tokens/main.py
+++ tokens/main.py
@@ -1,30 +1,55 @@
 """Copies Hail tokens from Kubernetes to the Google Secret Manager."""
 
+import base64
 import json
-from kubernetes import client, config
+from typing import List, NamedTuple
+import kubernetes.client
+import kubernetes.config
 from google.cloud import secretmanager
 
-PROJECTS = [
-    'tob-wgs',
-]
 
-GCP_PROJECT = 'analysis-runner'
-SECRET_MANAGER_SECRET_NAME = 'hail-tokens'
+class DatasetConfig(NamedTuple):
+    """The configuration for a particular dataset:
+    gcp_project_id: The GCP project ID associated with the dataset.
+    allowed_repos: The repositories allowed for running analyses.
+    """
 
-config.load_kube_config()
-kube_client = client.CoreV1Api()
+    gcp_project_id: str
+    allowed_repos: List[str]
 
-tokens = {}
-for project in PROJECTS:
-    secret_name = f'{project}-tokens'
-    secret = kube_client.read_namespaced_secret(secret_name, 'default')
-    tokens[project] = secret.data['tokens.json']
+
+# Per-dataset configuration.
+CONFIG = {
+    'tob-wgs': DatasetConfig('tob-wgs', ['hail-batch-test']),
+    'fewgenomes': DatasetConfig('fewgenomes', ['hail-batch-test']),
+}
+
+kubernetes.config.load_kube_config()
+kube_client = kubernetes.client.CoreV1Api()
 
 secret_manager = secretmanager.SecretManagerServiceClient()
-PARENT = secret_manager.secret_path(GCP_PROJECT, SECRET_MANAGER_SECRET_NAME)
-payload = json.dumps(tokens).encode('UTF-8')
-response = secret_manager.add_secret_version(
-    request={'parent': PARENT, 'payload': {'data': payload}}
-)
 
-print(f'Added secret version: {response.name}')
+
+def add_secret(gcp_project_id: str, name: str, value: str) -> None:
+    """Adds the given secret to the Secret Manager as a new version."""
+    payload = value.encode('UTF-8')
+    secret_path = secret_manager.secret_path(gcp_project_id, name)
+    response = secret_manager.add_secret_version(
+        request={'parent': secret_path, 'payload': {'data': payload}}
+    )
+    print(response.name)
+
+
+for dataset_name, dataset_config in CONFIG.items():
+    kube_secret_name = f'{dataset_name}-tokens'
+    kube_secret = kube_client.read_namespaced_secret(kube_secret_name, 'default')
+    secret_data = kube_secret.data['tokens.json']
+    hail_token = json.loads(base64.b64decode(secret_data))['default']
+
+    add_secret(dataset_config.gcp_project_id, 'hail-token', hail_token)
+
+    add_secret(
+        dataset_config.gcp_project_id,
+        'allowed-repos',
+        ','.join(dataset_config.allowed_repos),
+    )
