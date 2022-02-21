c539c30970be9944cdb238102ee4244a6ba7c7e4
Vlad Savelyev <vladislav.savelyev@populationgenomics.org.au>
2022-02-21 18:37:15 +1100

    Allow view cpg_reference bucket for access group (#330)
    * Allow view cpg_reference bucket for access group
    
    * Typo

--- stack/__main__.py
+++ stack/__main__.py
@@ -601,6 +601,14 @@ def main():  # pylint: disable=too-many-locals,too-many-branches
         member=pulumi.Output.concat('group:', access_group.group_key.id),
     )
 
+    # Read access to reference data.
+    bucket_member(
+        'access-group-reference-bucket-viewer',
+        bucket=REFERENCE_BUCKET_NAME,
+        role=viewer_role_id,
+        member=pulumi.Output.concat('group:', access_group.group_key.id),
+    )
+
     # Allow the usage of requester-pays buckets.
     gcp.projects.IAMMember(
         f'access-group-serviceusage-consumer',
