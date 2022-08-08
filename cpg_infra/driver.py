a23dff3cad6c11be6d5ee46eaa6dbe4cb310b6c4
Michael Franklin <michael.franklin@populationgenomics.org.au>
2022-08-09 01:52:43 +1000

    More driver implementation

--- cpg_infra/driver.py
+++ cpg_infra/driver.py
@@ -9,8 +9,13 @@ import pulumi
 
 from cpg_infra.abstraction.azure import AzureInfra
 from cpg_infra.abstraction.gcp import GcpInfrastructure
-from cpg_infra.abstraction.base import CloudInfraBase, DevInfra, SecretMembership
-from cpg_infra.config import CPGDatasetConfig
+from cpg_infra.abstraction.base import (
+    CloudInfraBase,
+    DevInfra,
+    SecretMembership,
+    BucketPermission,
+)
+from cpg_infra.config import CPGDatasetConfig, CPGDatasetComponents
 
 DOMAIN = "populationgenomics.org.au"
 CUSTOMER_ID = "C010ys3gt"
@@ -60,13 +65,23 @@ SAMPLE_METADATA_PERMISSIONS = [
 
 AccessLevel = str
 ACCESS_LEVELS: Iterable[AccessLevel] = ("test", "standard", "full")
-NON_NAME_REGEX = re.compile(r'[^A-Za-z0-9_-]')
+NON_NAME_REGEX = re.compile(r"[^A-Za-z0-9_-]")
+
+
 
 
 class CPGInfrastructure:
     def __init__(self, infra, config: CPGDatasetConfig):
         self.config: CPGDatasetConfig = config
-        self.infra: CloudInfraBase = infra(config) if isclass(infra) else infra
+        self.infra: CloudInfraBase = infra(self.config) if isclass(infra) else infra
+        self.components: list[CPGDatasetComponents] = config.components.get(self.infra.name(), CPGDatasetComponents.default_component_for_infrastructure()[self.infra.name()])
+
+    def create_group(self, name: str):
+        group = self.infra.create_group(name)
+        self.infra.add_group_member(
+            f"access-group-cacher-{name}", group, ACCESS_GROUP_CACHE_SERVICE_ACCOUNT
+        )
+        return group
 
     def main(self):
 
@@ -75,14 +90,42 @@ class CPGInfrastructure:
         self.setup_dependent_group_memberships()
 
         # setup bucket permissions
-        self.setup_main_bucket_permissions()
+        if CPGDatasetComponents.STORAGE in self.components:
+            self.setup_main_bucket_permissions()
+            self.setup_main_tmp_bucket()
+            self.setup_main_analysis_bucket()
+            self.setup_main_web_bucket_permissions()
+            self.setup_main_upload_buckets_permissions()
+            self.setup_test_buckets_permissions()
 
-        self.setup_sample_metadata_permissions()
+            if self.config.enable_release:
+                self.setup_release_bucket_permissions()
 
-        self.setup_main_upload_buckets()
+        if CPGDatasetComponents.SAMPLE_METADATA in self.components:
+
+            # sample-metadata
+            self.setup_sample_metadata_permissions()
+
+        if CPGDatasetComponents.CROMWELL in self.components:
+            self.setup_cromwell_machine_accounts()
+
+        if isinstance(self.infra, GcpInfrastructure):
+            self._gcp_extra_steps()
 
         # outputs
         self.setup_access_level_group_outputs()
+
+    # region GCP SPECIFIC
+
+    def _gcp_extra_steps(self):
+        if 'azure' in self.config.deploy_locations:
+            # we'll set up GCP service-accounts for use by Azure SAs, with credentials on SM
+            pass
+
+        pass
+
+
+    # endregion GCP SPECIFIC
 
     # region MACHINE ACCOUNTS
 
@@ -90,6 +133,11 @@ class CPGInfrastructure:
     @lru_cache()
     def main_upload_account(self):
         return self.infra.create_machine_account("main-upload")
+
+    @property
+    @lru_cache()
+    def notebook_account(self):
+        pass
 
     @property
     @lru_cache()
@@ -115,6 +163,14 @@ class CPGInfrastructure:
             for access_level, machine_account in values:
                 yield kind, access_level, machine_account
 
+    def working_machine_accounts_by_access_level(self):
+        machine_accounts: dict[AccessLevel, list[any]] = defaultdict(list)
+        for kind, values in self.working_machine_accounts_by_type.items():
+            for access_level, machine_account in values:
+                machine_accounts[access_level].append(machine_account)
+
+        return machine_accounts
+
     def _get_hail_and_deploy_accounts(self) -> dict[str, list[tuple[AccessLevel, Any]]]:
         service_accounts = defaultdict(list)
         for kind in "hail", "deployment":
@@ -131,7 +187,13 @@ class CPGInfrastructure:
         self,
     ) -> dict[str, list[tuple[AccessLevel, Any]]]:
         service_accounts = defaultdict(list)
-        for kind in "dataproc", "cromwell":
+        kinds = []
+        if CPGDatasetComponents.SPARK in self.components:
+            kinds.append('dataproc')
+        if CPGDatasetComponents.CROMWELL in self.components:
+            kinds.append('cromwell')
+
+        for kind in kinds:
             for access_level in ACCESS_LEVELS:
                 service_accounts[kind].append(
                     (
@@ -143,23 +205,27 @@ class CPGInfrastructure:
         return service_accounts
 
     # endregion MACHINE ACCOUNTS
-
     # region GROUPS
 
     @property
     @lru_cache
     def access_group(self):
-        return self.infra.create_group("access")
+        return self.create_group("access")
 
     @property
     @lru_cache()
     def web_access_group(self):
-        return self.infra.create_group("web-access")
+        return self.create_group("web-access")
+
+    @property
+    @lru_cache()
+    def release_access_group(self):
+        return self.create_group('release-access')
 
     @property
     @lru_cache()
     def access_level_groups(self) -> dict[AccessLevel, Any]:
-        return {al: self.infra.create_group(al) for al in ACCESS_LEVELS}
+        return {al: self.create_group(al) for al in ACCESS_LEVELS}
 
     @staticmethod
     def get_access_level_group_output_name(*, access_level: AccessLevel):
@@ -169,7 +235,7 @@ class CPGInfrastructure:
         for access_level, group in self.access_level_groups.items():
             pulumi.export(
                 self.get_access_level_group_output_name(access_level=access_level),
-                group.id if hasattr(group, 'id') else group,
+                group.id if hasattr(group, "id") else group,
             )
 
     def setup_access_level_group_memberships(self):
@@ -190,7 +256,7 @@ class CPGInfrastructure:
     def sample_metadata_groups(self) -> dict[str, any]:
         sm_groups = {}
         for key in SAMPLE_METADATA_PERMISSIONS:
-            sm_groups[key] = self.infra.create_group(f"sample-metadata-{key}")
+            sm_groups[key] = self.create_group(f"sample-metadata-{key}")
 
         return sm_groups
 
@@ -249,7 +315,7 @@ class CPGInfrastructure:
         for name, member, permission in sm_access_levels:
             for kind in permission:
                 self.infra.add_group_member(
-                    f'sample-metadata-{kind}-{name}-access-level-group-membership',
+                    f"sample-metadata-{kind}-{name}-access-level-group-membership",
                     self.sample_metadata_groups[kind],
                     member,
                 )
@@ -281,66 +347,251 @@ class CPGInfrastructure:
     # endregion GROUPS
     # region MAIN BUCKETS
 
+    def setup_main_bucket_permissions(self):
+        # access has list permission
+        self.infra.add_member_to_bucket(
+            "project-bucket-lister",
+            self.main_bucket,
+            self.access_group,
+            BucketPermission.LIST,
+        )
+
+        self.infra.add_member_to_bucket(
+            "standard-main-bucket-view-create",
+            self.main_bucket,
+            self.access_level_groups["standard"],
+            BucketPermission.APPEND,
+        )
+
+        self.infra.add_member_to_bucket(
+            "full-main-bucket-admin",
+            self.main_bucket,
+            self.access_level_groups["full"],
+            BucketPermission.MUTATE,
+        )
+
+    def setup_main_tmp_bucket(self):
+        self.infra.add_member_to_bucket(
+            "standard-main-tmp-bucket-view-create",
+            self.main_tmp_bucket,
+            self.access_level_groups["standard"],
+            BucketPermission.APPEND,
+        )
+
+        self.infra.add_member_to_bucket(
+            "full-main-tmp-bucket-admin",
+            self.main_tmp_bucket,
+            self.access_level_groups["full"],
+            BucketPermission.MUTATE,
+        )
+
+    def setup_main_analysis_bucket(self):
+        self.infra.add_member_to_bucket(
+            "access-group-main-analysis-bucket-viewer",
+            self.main_analysis_bucket,
+            self.access_group,
+            BucketPermission.READ,
+        )
+        self.infra.add_member_to_bucket(
+            "standard-main-analysis-bucket-view-create",
+            self.main_analysis_bucket,
+            self.access_level_groups["standard"],
+            BucketPermission.APPEND,
+        )
+
+        self.infra.add_member_to_bucket(
+            "full-main-analysis-bucket-admin",
+            self.main_analysis_bucket,
+            self.access_level_groups["full"],
+            BucketPermission.MUTATE,
+        )
+
+    def setup_main_web_bucket_permissions(self):
+        self.infra.add_member_to_bucket(
+            "access-group-main-web-bucket-viewer",
+            self.main_web_bucket,
+            self.access_group,
+            BucketPermission.READ,
+        )
+
+        # web-server
+        self.infra.add_member_to_bucket(
+            "web-server-main-web-bucket-viewer",
+            self.main_web_bucket,
+            WEB_SERVER_SERVICE_ACCOUNT,
+            BucketPermission.READ,
+        )
+
+        self.infra.add_member_to_bucket(
+            "standard-main-web-bucket-view-create",
+            self.main_web_bucket,
+            self.access_level_groups["standard"],
+            BucketPermission.APPEND,
+        )
+
+        self.infra.add_member_to_bucket(
+            "full-main-web-bucket-admin",
+            self.main_web_bucket,
+            self.access_level_groups["full"],
+            BucketPermission.APPEND,
+        )
+
     def setup_main_upload_buckets_permissions(self):
         for bname, main_upload_bucket in self.main_upload_buckets.items():
             self.infra.add_member_to_bucket(
                 f"main-upload-service-account-{bname}-bucket-creator",
                 bucket=main_upload_bucket,
                 member=self.main_upload_account,
+                membership=BucketPermission.MUTATE,
             )
-
-    def setup_main_bucket_permissions(self):
-        pass
 
     @property
     @lru_cache()
     def main_bucket(self):
-        return self.infra.create_bucket(
-            "main", lifecycle_rules=[self.infra.bucket_rule_undelete()]
-        )
+        return self.infra.create_bucket("main", lifecycle_rules=[self.infra.bucket_rule_undelete()])
 
     @property
     @lru_cache()
     def main_tmp_bucket(self):
-        return self.infra.create_bucket(
-            "main-tmp", lifecycle_rules=[self.infra.bucket_rule_temporary()]
-        )
+        return self.infra.create_bucket("main-tmp", lifecycle_rules=[self.infra.bucket_rule_temporary()])
 
     @property
     @lru_cache()
-    def main_analysis(self):
-        return self.infra.create_bucket(
-            "main-analysis", lifecycle_rules=[self.infra.bucket_rule_undelete()]
-        )
+    def main_analysis_bucket(self):
+        return self.infra.create_bucket("main-analysis", lifecycle_rules=[self.infra.bucket_rule_undelete()])
 
     @property
     @lru_cache()
-    def main_web(self):
-        return self.infra.create_bucket(
-            "main-web", lifecycle_rules=[self.infra.bucket_rule_undelete()]
-        )
+    def main_web_bucket(self):
+        return self.infra.create_bucket("main-web", lifecycle_rules=[self.infra.bucket_rule_undelete()])
 
     @property
     @lru_cache()
     def main_upload_buckets(self) -> dict[str, Any]:
         main_upload_undelete = self.infra.bucket_rule_undelete(days=30)
         main_upload_buckets = {
-            "main-upload": self.infra.create_bucket(
-                "main-upload", lifecycle_rules=[main_upload_undelete]
-            )
+            "main-upload": self.infra.create_bucket("main-upload", lifecycle_rules=[main_upload_undelete])
         }
 
         for additional_upload_bucket in self.config.additional_upload_buckets:
-            main_upload_buckets[additional_upload_bucket] = self.infra.create_bucket(
-                additional_upload_bucket,
-                lifecycle_rules=[main_upload_undelete],
-                unique=True,
-            )
+            main_upload_buckets[additional_upload_bucket] = self.infra.create_bucket(additional_upload_bucket,
+                                                                                     lifecycle_rules=[
+                                                                                         main_upload_undelete],
+                                                                                     unique=True)
 
         return main_upload_buckets
 
     # endregion MAIN BUCKETS
+    # region TEST BUCKETS
 
+    def setup_test_buckets_permissions(self):
+        """
+        Test bucket permissions are much more uniform,
+        so just work out some more generic mechanism
+        """
+
+        buckets = [
+            ('test', self.test_bucket),
+            ('test-analysis', self.test_analysis_bucket),
+            ('test-tmp', self.test_tmp_bucket),
+            ('test-web', self.test_web_bucket),
+        ]
+
+        for bucket_name, bucket in buckets:
+
+            test_bucket_admins: list[tuple[str, Any]] = [
+                (f'access-group-{bucket_name}-bucket-admin', self.access_group),
+                *[
+                    (f"{access_level}-test-bucket-admin", group)
+                    for access_level, group in self.access_level_groups.items()
+                ],
+            ]
+            for resource_key, group in test_bucket_admins:
+                self.infra.add_member_to_bucket(
+                    resource_key,
+                    self.test_bucket,
+                    group,
+                    BucketPermission.MUTATE,
+                )
+
+    @property
+    @lru_cache()
+    def test_bucket(self):
+        return self.infra.create_bucket("test", lifecycle_rules=[self.infra.bucket_rule_undelete()])
+
+    @property
+    @lru_cache()
+    def test_analysis_bucket(self):
+        return self.infra.create_bucket("test-analysis", lifecycle_rules=[self.infra.bucket_rule_undelete()])
+
+    @property
+    @lru_cache()
+    def test_web_bucket(self):
+        return self.infra.create_bucket("test-web", lifecycle_rules=[self.infra.bucket_rule_undelete()])
+
+    @property
+    @lru_cache()
+    def test_tmp_bucket(self):
+        return self.infra.create_bucket("test-tmp", lifecycle_rules=[self.infra.bucket_rule_temporary()])
+
+    # endregion TEST BUCKETS
+    # region RELEASE BUCKETS
+
+    def setup_release_bucket_permissions(self):
+        self.infra.add_member_to_bucket('access-group-release-bucket-viewer', self.release_bucket, self.access_group, BucketPermission.READ)
+
+        self.infra.add_member_to_bucket(
+            'release-access-group-release-bucket-viewer',
+            self.release_bucket,
+            self.release_access_group,
+            BucketPermission.READ
+        )
+
+        self.infra.add_member_to_bucket(
+            'full-release-bucket-admin',
+            self.release_bucket,
+            self.access_level_groups['full'],
+            BucketPermission.MUTATE
+        )
+
+    @property
+    @lru_cache()
+    def release_bucket(self):
+        return self.infra.create_bucket('release-requester-pays', lifecycle_rules=[self.infra.bucket_rule_undelete()])
+    # endregion RELEASE BUCKETS
+    # region CROMWELL
+
+    def setup_cromwell_machine_accounts(self):
+        cromwell_machine_accounts = self.working_machine_accounts_by_type.get('cromwell')
+        if not cromwell_machine_accounts:
+            raise ValueError('This method may be called where cromwell machine accounts are not activated')
+
+        for access_level, machine_account in cromwell_machine_accounts:
+
+            # To use a service account for VMs, Cromwell accounts need
+            # to be allowed to use themselves ;)
+            self.infra.add_member_to_machine_account_access(
+                f'cromwell-service-account-{access_level}-service-account-user',
+                machine_account,
+                machine_account,
+            )
+
+            # TODO: test if this is necessary, I don't think it should be :suss:
+            # Allow the Cromwell SERVER to run worker VMs using the Cromwell SAs
+            self.infra.add_member_to_machine_account_access(
+                f'cromwell-runner-{access_level}-service-account-user',
+                machine_account,
+                CROMWELL_RUNNER_ACCOUNT
+            )
+
+    def _GCP_setup_cromwell(self):
+        assert isinstance(self.infra, GcpInfrastructure)
+        # Allow the Cromwell service accounts to run workflows.
+
+
+
+
+    # endregion CROMWELL
     # region DEPENDENCIES
 
     def setup_dependent_group_memberships(self):
@@ -363,9 +614,9 @@ class CPGInfrastructure:
         return pulumi.StackReference(dependency_name)
 
     # endregion DEPENDENCIES
-
+    # region UTILS
     @staticmethod
-    def _get_name_from_external_sa(email: str, suffix='.iam.gserviceaccount.com'):
+    def _get_name_from_external_sa(email: str, suffix=".iam.gserviceaccount.com"):
         """
         Convert service account email to name + some filtering.
 
@@ -381,9 +632,11 @@ class CPGInfrastructure:
         if email.endswith(suffix):
             base = email[: -len(suffix)]
         else:
-            base = email.split('@')[0]
+            base = email.split("@")[0]
 
-        return NON_NAME_REGEX.sub('-', base).replace('--', '-')
+        return NON_NAME_REGEX.sub("-", base).replace("--", "-")
+
+    # endregion UTILS
 
 
 if __name__ == "__main__":
