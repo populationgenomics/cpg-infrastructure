ce1bc7374136cba6e827d18656213033e870ad9a
Michael Franklin <michael.franklin@populationgenomics.org.au>
2022-08-21 21:47:21 +1000

    Use InfrastructureConfig for pulumi

--- cpg_infra/__main__.py
+++ cpg_infra/__main__.py
@@ -1,5 +1,11 @@
 import pulumi
-from cpg_infra.driver import CPGInfrastructure, CPGDatasetConfig
+from cpg_utils.config import get_config
+
+from cpg_infra.driver import (
+    CpgDatasetInfrastructure,
+    CPGInfrastructureConfig,
+    CPGDatasetConfig,
+)
 
 # NOTE: Uncomment the below code when launching pulumi locally
 # after running `pulumi up` or an equivalent command, then hit F5 to connect the
@@ -16,8 +22,9 @@ from cpg_infra.driver import CPGInfrastructure, CPGDatasetConfig
 
 def from_pulumi():
     pconfig = pulumi.Config()
-    config = CPGDatasetConfig.from_pulumi(pconfig, dataset=pulumi.get_stack())
-    CPGInfrastructure.deploy_all_from_config(config)
+    config = CPGInfrastructureConfig.from_dict(get_config())
+    dataset_config = CPGDatasetConfig.from_pulumi(pconfig, dataset=pulumi.get_stack())
+    CpgDatasetInfrastructure.deploy_all_from_config(config, dataset_config)
 
 
 if __name__ == '__main__':
