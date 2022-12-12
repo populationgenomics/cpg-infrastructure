3dedcf74818e5d8b1afab36b94ca8527f0c8aa98
Michael Franklin <michael.franklin@populationgenomics.org.au>
2022-12-12 14:17:45 +1100

    Refactor tokens/main.py to read from production.yaml

--- tokens/main.py
+++ tokens/main.py
@@ -1,9 +1,11 @@
 # pylint: disable=import-error
 """Copies Hail tokens from Kubernetes to the Google Secret Manager."""
 
+from typing import Any
 import base64
 import json
 from pathlib import Path
+from collections import defaultdict
 
 import kubernetes.client
 import kubernetes.config
@@ -39,8 +41,14 @@ def add_secret(name: str, value: str) -> None:
     print(response.name)
 
 
-def get_token(hail_user: str) -> str:
+def get_token(environment: str, hail_user: str) -> str:
     """Returns the Hail token for the given user."""
+
+    if not environment == 'gcp':
+        raise NotImplementedError(
+            f'Cannot get token for hail_user {hail_user} on {environment}'
+        )
+
     kube_secret_name = f'{hail_user}-tokens'
     kube_secret = kube_client.read_namespaced_secret(kube_secret_name, 'default')
     secret_data = kube_secret.data['tokens.json']
@@ -48,45 +56,55 @@ def get_token(hail_user: str) -> str:
     return hail_token
 
 
-def get_project_id(dataset: str) -> str:
-    """Returns the GCP project ID associated with the given dataset."""
-    with open(REPO_ROOT / f'stack/Pulumi.{dataset}.yaml', encoding='utf-8') as f:
-        return yaml.safe_load(f)['config']['gcp:project']
-
-
-def get_hail_user(dataset: str, access_level: str) -> str | None:
-    """
-    Returns the hail user associated with the given dataset for the access level
-    """
-    with open(REPO_ROOT / f'stack/Pulumi.{dataset}.yaml', encoding='utf-8') as f:
-        config = yaml.safe_load(f)['config']
-        key = f'datasets:gcp_hail_service_account_{access_level}'
-        val = config.get(key)
-        if not val:
-            return None
-        # removes -\d{3}@hail-295901.iam.gserviceaccount.com
-        service_account_name = config[key][:-40]
-        return service_account_name
-
-
 def main():
     """Main entry point."""
 
+    with open('../stack/production.yaml') as f:
+        production_config = yaml.safe_load(f)
+
+    invalid_datasets = set(ALLOWED_REPOS.keys()) - set(production_config.keys())
+    if invalid_datasets:
+        raise ValueError(
+            'Some datasets in allowed_repos were not contained in production.yaml: '
+            + ', '.join(invalid_datasets)
+        )
+
     config = {}
-    for dataset, allowed_repos in ALLOWED_REPOS.items():
-        entries = {
-            'projectId': get_project_id(dataset),
-            'allowedRepos': list(set(ALWAYS_ALLOWED_REPOS + allowed_repos)),
-        }
-        for access_level in 'test', 'standard', 'full':
-            hail_user = get_hail_user(dataset, access_level)
-            if not hail_user:
-                print(f'Warning: no Hail user found for {dataset}/{access_level}')
-                entries = None
-                break
-            entries[f'{access_level}Token'] = get_token(hail_user)
-        if entries:
-            config[dataset] = entries
+
+    for dataset, dataset_config in production_config.items():
+        allowed_repos = ALLOWED_REPOS.get(dataset, [])
+        gcp_project_id = dataset_config.get('gcp', {}).get('project')
+        if not gcp_project_id:
+            raise ValueError(
+                f'Could not find GCP project ID for {dataset} in production.yaml'
+            )
+
+        entries: dict[str, dict[str, Any] | list[str]] = defaultdict(dict)
+
+        for infra in ('gcp',):
+            entries[infra] = {}
+            infra_config = dataset_config.get(infra)
+            if not infra_config:
+                continue
+
+            project_key = 'project' if infra == 'gcp' else 'subscriptionId'
+            entries[infra]['projectId'] = infra_config.get(project_key)
+            for access_level in 'test', 'standard', 'full':
+
+                # removes -\d{3}@hail-295901.iam.gserviceaccount.com
+                hail_user = infra_config.get(
+                    f'hail_service_account_{access_level}', ''
+                )[:-40]
+                if not hail_user:
+                    print(
+                        f'Warning: no Hail user found for {infra}/{dataset}/{access_level}'
+                    )
+                    break
+                entries[infra][f'{access_level}Token'] = get_token(
+                    environment=infra, hail_user=hail_user
+                )
+        entries['allowedRepos'] = list(set(ALWAYS_ALLOWED_REPOS + allowed_repos))
+        config[dataset] = entries
 
     add_secret('server-config', json.dumps(config))
 
