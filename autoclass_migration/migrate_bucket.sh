cb09471fd0aa8c2c55c595d04e162d227dd8e542
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2023-02-06 09:10:09 +1100

    Fixes, thanks ShellCheck!

--- autoclass_migration/migrate_bucket.sh
+++ autoclass_migration/migrate_bucket.sh
@@ -27,7 +27,7 @@ post_to_slack() {
     curl --fail --silent --show-error  -X POST \
         -H 'Content-type: application/json' \
         -d '{"text":"'"$1"'"}' \
-        $SLACK_WEBHOOK
+        "$SLACK_WEBHOOK"
 }
 
 report_exit_status() {
@@ -47,88 +47,88 @@ trap "report_exit_status" EXIT
 post_to_slack "Starting migration for $BUCKET"
 
 # First check that Autoclass hasn't already been enabled.
-if gsutil -u $BILLING_PROJECT autoclass get gs://$BUCKET | grep True; then
+if gsutil -u "$BILLING_PROJECT" autoclass get "gs://$BUCKET" | grep True; then
     post_to_slack "Autoclass is already enabled for $BUCKET"
     exit 0
 fi
 
 # Store the IAM permissions.
-gsutil -u $BILLING_PROJECT iam get gs://$BUCKET > /tmp/iam.json
+gsutil -u "$BILLING_PROJECT" iam get "gs://$BUCKET" > /tmp/iam.json
 
 # Store the object versioning setting.
-OBJECT_VERSIONING=$(gsutil -u $BILLING_PROJECT versioning get gs://$BUCKET | cut -f 2 -d ' ')
+OBJECT_VERSIONING=$(gsutil -u "$BILLING_PROJECT" versioning get "gs://$BUCKET" | cut -f 2 -d ' ')
 
 # Store the lifecycle configuration.
-gsutil -u $BILLING_PROJECT lifecycle get gs://$BUCKET > /tmp/lifecycle_config.json
+gsutil -u "$BILLING_PROJECT" lifecycle get "gs://$BUCKET" > /tmp/lifecycle_config.json
 
 # Store the Requester Pays setting.
-REQUESTER_PAYS=$(gsutil -u $BILLING_PROJECT requesterpays get gs://cpg-fewgenomes-test | cut -f 2 -d ' ')
+REQUESTER_PAYS=$(gsutil -u "$BILLING_PROJECT" requesterpays get gs://cpg-fewgenomes-test | cut -f 2 -d ' ')
 
 # Remove all IAM permissions to prevent modifications while we perform the temporary copy.
-gsutil -u $BILLING_PROJECT iam set -e '' <(echo "{}") gs://$BUCKET
+gsutil -u "$BILLING_PROJECT" iam set -e '' <(echo "{}") "gs://$BUCKET"
 
 # Determine total size.
-BUCKET_SIZE=$(gsutil -u $BILLING_PROJECT du -s gs://$BUCKET | cut -f 1 -d ' ')
+BUCKET_SIZE=$(gsutil -u "$BILLING_PROJECT" du -s "gs://$BUCKET" | cut -f 1 -d ' ')
 post_to_slack "Bucket size for $BUCKET: $BUCKET_SIZE B"
 
 # Only need to perform a copy if the bucket is non-empty.
-if [[ BUCKET_SIZE -gt 0 ]]; then
+if [[ $BUCKET_SIZE -gt 0 ]]; then
     # Create a temporary bucket.
     TMP_BUCKET=$BUCKET-autoclass-migration-tmp
-    gsutil --project=$GCP_PROJECT -u $BILLING_PROJECT mb gs://$TMP_BUCKET \
+    gsutil --project="$GCP_PROJECT" -u "$BILLING_PROJECT" mb "gs://$TMP_BUCKET" \
         -l australia-southeast1 \
         -b on
 
     # Copy all data to the temporary bucket.
-    gsutil -u $BILLING_PROJECT -m cp -r "gs://$BUCKET/*" "gs://$TMP_BUCKET"
+    gsutil -u "$BILLING_PROJECT" -m cp -r "gs://$BUCKET/*" "gs://$TMP_BUCKET"
 
     # Compare total bucket sizes to make sure the copy completed successfully.
-    TMP_BUCKET_SIZE=$(gsutil -u $BILLING_PROJECT du -s gs://$TMP_BUCKET | cut -f 1 -d ' ')
-    if [[ BUCKET_SIZE -ne TMP_BUCKET_SIZE ]]; then
+    TMP_BUCKET_SIZE=$(gsutil -u "$BILLING_PROJECT" du -s "gs://$TMP_BUCKET" | cut -f 1 -d ' ')
+    if [[ $BUCKET_SIZE -ne $TMP_BUCKET_SIZE ]]; then
         post_to_slack "Temporary copy size mismatch for $BUCKET: $BUCKET_SIZE B vs $TMP_BUCKET_SIZE B"
         exit 1
     fi
 fi
 
 # Delete the original bucket.
-gsutil -u $BILLING_PROJECT -m rm -r gs://$BUCKET
+gsutil -u "$BILLING_PROJECT" -m rm -r "gs://$BUCKET"
 
 # Recreate the bucket, this time with Autoclass enabled.
-gsutil --project=$GCP_PROJECT -u $BILLING_PROJECT mb gs://$BUCKET \
+gsutil --project="$GCP_PROJECT" -u "$BILLING_PROJECT" mb "gs://$BUCKET" \
     -l australia-southeast1 \
     -b on \
     --autoclass
 
 # Only need to perform a copy if the bucket is non-empty.
-if [[ BUCKET_SIZE -gt 0 ]]; then
+if [[ $BUCKET_SIZE -gt 0 ]]; then
     # Copy all data to back from the temporary bucket.
-    gsutil -u $BILLING_PROJECT -m cp -r "gs://$TMP_BUCKET/*" "gs://$BUCKET"
+    gsutil -u "$BILLING_PROJECT" -m cp -r "gs://$TMP_BUCKET/*" "gs://$BUCKET"
 
     # Compare total bucket sizes to make sure the copy completed successfully.
-    BUCKET_SIZE=$(gsutil -u $BILLING_PROJECT du -s gs://$BUCKET | cut -f 1 -d ' ')
-    if [[ BUCKET_SIZE -ne TMP_BUCKET_SIZE ]]; then
+    BUCKET_SIZE=$(gsutil -u "$BILLING_PROJECT" du -s "gs://$BUCKET" | cut -f 1 -d ' ')
+    if [[ $BUCKET_SIZE -ne $TMP_BUCKET_SIZE ]]; then
         post_to_slack "Back-copy size mismatch for $BUCKET: $BUCKET_SIZE B vs $TMP_BUCKET_SIZE B"
         exit 1
     fi
 
     # Delete the temporary bucket.
-    gsutil -u $BILLING_PROJECT -m rm -r gs://$TMP_BUCKET
+    gsutil -u "$BILLING_PROJECT" -m rm -r "gs://$TMP_BUCKET"
 fi
 
 # Restore object versioning.
-if [[ OBJECT_VERSIONING = "Enabled" ]]; then
-    gsutil -u $BILLING_PROJECT versioning set on gs://$BUCKET
+if [[ $OBJECT_VERSIONING = "Enabled" ]]; then
+    gsutil -u "$BILLING_PROJECT" versioning set on "gs://$BUCKET"
 fi
 
 # Restore the lifecycle configuration.
 if ! grep "has no lifecycle configuration" /tmp/lifecycle_config.json; then
-    gsutil -u $BILLING_PROJECT lifecycle set /tmp/lifecycle_config.json gs://$BUCKET
+    gsutil -u "$BILLING_PROJECT" lifecycle set /tmp/lifecycle_config.json "gs://$BUCKET"
 fi
 
 # Restore the Requester Pays setting.
-if [[ REQUESTER_PAYS = "Enabled" ]]; then
-    gsutil -u $BILLING_PROJECT requesterpays set on gs://$BUCKET
+if [[ $REQUESTER_PAYS = "Enabled" ]]; then
+    gsutil -u "$BILLING_PROJECT" requesterpays set on "gs://$BUCKET"
 fi
 
 # Restore the IAM permissions.
-gsutil -u $BILLING_PROJECT iam set -e '' /tmp/iam.json gs://$BUCKET
+gsutil -u "$BILLING_PROJECT" iam set -e '' /tmp/iam.json "gs://$BUCKET"
