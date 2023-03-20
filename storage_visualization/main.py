7570eaf2ffdf077f57a52b00c7ebe331402b6c09
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2023-03-20 21:43:00 +1100

    Add Slack notification

--- storage_visualization/main.py
+++ storage_visualization/main.py
@@ -6,6 +6,7 @@ import sys
 
 import hailtop.batch as hb
 
+from cpg_utils.config import get_config
 from cpg_utils.git import (
     prepare_git_job,
     get_git_commit_ref_of_current_repository,
@@ -17,21 +18,24 @@ from cpg_utils.hail_batch import (
     copy_common_env,
     remote_tmpdir,
     output_path,
+    web_url,
 )
-from cpg_utils.config import get_config
+from cpg_utils.slack import slack_message_cmd
 
 
-def prepare_job(job):
+def prepare_job(job, clone_repository):
     """Sets up the given job to run scripts in the same repository."""
     job.image(get_config()['workflow']['driver_image'])
     copy_common_env(job)
     authenticate_cloud_credentials_in_job(job)
-    prepare_git_job(
-        job=job,
-        organisation=get_organisation_name_from_current_directory(),
-        repo_name=get_repo_name_from_current_directory(),
-        commit=get_git_commit_ref_of_current_repository(),
-    )
+
+    if clone_repository:
+        prepare_git_job(
+            job=job,
+            organisation=get_organisation_name_from_current_directory(),
+            repo_name=get_repo_name_from_current_directory(),
+            commit=get_git_commit_ref_of_current_repository(),
+        )
 
 
 def main():
@@ -49,8 +53,8 @@ def main():
     # Process all datasets in parallel, as separate jobs.
     job_output_paths = {}
     for dataset in sys.argv[1:]:
-        job = batch.new_job(name=f'Process {dataset}')
-        prepare_job(job)
+        job = batch.new_job(name=f'process-{dataset}')
+        prepare_job(job, clone_repository=True)
 
         path = output_path(f'{dataset}.json.gz', dataset='common', category='analysis')
 
@@ -58,15 +62,24 @@ def main():
 
         job_output_paths[job] = path
 
-    # Process the combined output of all jobs.
-    treemap_job = batch.new_job(name='Combined treemap')
-    prepare_job(treemap_job)
+    # Process the combined output of all jobs to generate a web report.
+    treemap_job = batch.new_job(name='treemap')
+    prepare_job(treemap_job, clone_repository=True)
     for job in job_output_paths:
         treemap_job.depends_on(job)
 
     web_path = output_path('treemap.html', dataset='common', category='web')
     treemap_job.command(
         f'storage_visualization/treemap.py --output {web_path} --group-by-dataset {" ".join(f"--input {path}" for path in job_output_paths.values())}'
+    )
+
+    # Send a Slack message when the HTML page has been generated.
+    slack_job = batch.new_job(name='slack')
+    prepare_job(slack_job, clone_repository=False)
+    slack_job.depends_on(treemap_job)
+    slack_message_cmd(
+        slack_job,
+        text=f'New storage visualization: {web_url("treemap.html", dataset="common")}',
     )
 
     batch.run(wait=False)
