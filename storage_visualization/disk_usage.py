63b07cd79a357d672f09e4a0d285457e93c7a630
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2023-03-22 17:19:47 +1100

    Add storage visualization automation (#105)
    * wip
    
    * test
    
    * Fix analysis-runner invocation
    
    * Specify job image
    
    * Use absolute paths
    
    * Add slack config
    
    * Add Slack notification
    
    * Add workflow
    
    * Use send_message for Slack notification
    
    * Use run_analysis_runner method instead of subprocess
    
    * Fix treemap writing
    
    * Less CPU
    
    * Add lint requirements
    
    * Use team-data channel
    
    * Relax version requirements to avoid infinite Hail package fetching
    
    * Revert "Relax version requirements to avoid infinite Hail package fetching"
    
    This reverts commit e66b66f28c3ece70bb84030479c793d89628ead6.
    
    * Fix Hail version to avoid infinite package fetching
    
    * Don't install hail
    
    * Silence linter
    
    * Another linter attempt
    
    * EOL

---
+++ storage_visualization/disk_usage.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+
+"""Computes aggregate bucket disk usage stats."""
+
+from collections import defaultdict
+import gzip
+import json
+import logging
+import sys
+from cloudpathlib import AnyPath
+from cpg_utils.hail_batch import get_config
+from google.cloud import storage
+
+
+# It's important not to list the `archive` bucket here, as Class B operations are very
+# expensive for that storage class.
+BUCKET_SUFFIXES = [
+    'main',
+    'main-analysis',
+    'main-tmp',
+    'main-upload',
+    'main-web',
+    'test',
+    'test-analysis',
+    'test-tmp',
+    'test-upload',
+    'test-web',
+]
+
+STORAGE_COST_MONTHLY_PER_GB_SYDNEY = {
+    'STANDARD': 0.023,
+    'NEARLINE': 0.016,
+    'COLDLINE': 0.006,
+    'ARCHIVE': 0.0025,
+}
+
+
+def aggregate_level(name: str) -> str:
+    """
+    Returns a prefix for the given blob name at the folder or Hail table level.
+
+    >>> aggregate_level('some/folder/and/a/filename.bam')
+    'some/folder/and/a'
+    >>> aggregate_level('some/folder/and/a/hail_table.ht/index/files/part12345')
+    'some/folder/and/a/hail_table.ht'
+    >>> aggregate_level('some/folder/and/a/hail_matrix_table.mt/index/files/part12345')
+    'some/folder/and/a/hail_matrix_table.mt'
+    >>> aggregate_level('file_in_root.cram')
+    """
+    if (ht_index := name.find('.ht/')) != -1:
+        return name[: ht_index + 3]
+    if (mt_index := name.find('.mt/')) != -1:
+        return name[: mt_index + 3]
+    if (slash_index := name.rfind('/')) != -1:
+        return name[:slash_index]
+    return ''  # Root level
+
+
+def main():
+    """Main entrypoint."""
+    if len(sys.argv) != 3:
+        print('Usage: disk_usage.py <dataset> <output.json.gz>')
+        sys.exit(1)
+
+    # Don't print DEBUG logs from urllib3.connectionpool.
+    logging.getLogger().setLevel(logging.INFO)
+
+    storage_client = storage.Client()
+    dataset = sys.argv[1]
+    access_level = get_config()['workflow']['access_level']
+
+    aggregate_stats = defaultdict(lambda: defaultdict(int))
+    for bucket_suffix in BUCKET_SUFFIXES:
+        if access_level == 'test' and not bucket_suffix.startswith('test'):
+            continue  # Skip main buckets when testing.
+
+        bucket_name = f'cpg-{dataset}-{bucket_suffix}'
+        logging.info(f'Listing blobs in {bucket_name}...')
+        blobs = storage_client.list_blobs(bucket_name)
+        count = 0
+        for blob in blobs:
+            count += 1
+            if count % 10**6 == 0:
+                logging.info(f'{count // 10**6} M blobs...')
+            folder = f'/{aggregate_level(blob.name)}'
+            while True:
+                path = f'gs://{bucket_name}{folder}'
+                stats = aggregate_stats[path]
+                stats['size'] += blob.size
+                stats[f'{blob.storage_class}_bytes'] += blob.size
+                stats['num_blobs'] += 1
+                size_in_gb = blob.size / 2**30
+                stats['monthly_storage_cost'] += (  # Assumes the bucket is in Sydney.
+                    size_in_gb * STORAGE_COST_MONTHLY_PER_GB_SYDNEY[blob.storage_class]
+                )
+                if not folder:
+                    break
+                folder = folder[: folder.rfind('/')]
+
+        logging.info(f'{bucket_name} contains {count} blobs.')
+
+    output = sys.argv[2]
+    logging.info(f'Writing results to {output}...')
+    with AnyPath(output).open('wb') as f:
+        with gzip.open(f, 'wt') as gzf:
+            json.dump(aggregate_stats, gzf)
+
+
+if __name__ == '__main__':
+    main()
---
+++ storage_visualization/main.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+
+"""Main entrypoint for the storage visualization driver."""
+
+import sys
+
+# See requirements.txt for why we're disabling the linter warnings here.
+import hailtop.batch as hb  # pylint: disable=import-error
+
+from cpg_utils.config import get_config
+from cpg_utils.git import (
+    prepare_git_job,
+    get_git_commit_ref_of_current_repository,
+    get_organisation_name_from_current_directory,
+    get_repo_name_from_current_directory,
+)
+from cpg_utils.hail_batch import (
+    authenticate_cloud_credentials_in_job,
+    copy_common_env,
+    remote_tmpdir,
+    output_path,
+    web_url,
+)
+from cpg_utils.slack import send_message
+
+
+def prepare_job(job):
+    """Sets up the given job to run scripts in the same repository."""
+    job.image(get_config()['workflow']['driver_image'])
+    copy_common_env(job)
+    authenticate_cloud_credentials_in_job(job)
+    prepare_git_job(
+        job=job,
+        organisation=get_organisation_name_from_current_directory(),
+        repo_name=get_repo_name_from_current_directory(),
+        commit=get_git_commit_ref_of_current_repository(),
+    )
+
+
+def main():
+    """Main entrypoint."""
+    if len(sys.argv) < 2:
+        print('Usage: main.py <dataset1> <dataset2> ...')
+        sys.exit(1)
+
+    service_backend = hb.ServiceBackend(
+        billing_project=get_config()['hail']['billing_project'],
+        remote_tmpdir=remote_tmpdir(),
+    )
+    batch = hb.Batch(name='Storage visualization driver', backend=service_backend)
+
+    # Process all datasets in parallel, as separate jobs.
+    job_output_paths = {}
+    for dataset in sys.argv[1:]:
+        job = batch.new_job(name=f'process-{dataset}')
+        prepare_job(job)
+
+        # Reading all blob metadata is expensive and can take a long time, so don't risk
+        # getting preempted.
+        job._preemptible = False  # pylint: disable=protected-access
+        job.cpu(0.5)
+
+        path = output_path(f'{dataset}.json.gz', dataset='common', category='analysis')
+        job.command(f'storage_visualization/disk_usage.py {dataset} {path}')
+
+        job_output_paths[job] = path
+
+    # Process the combined output of all jobs to generate a web report.
+    treemap_job = batch.new_job(name='treemap')
+    prepare_job(treemap_job)
+    for job in job_output_paths:
+        treemap_job.depends_on(job)
+
+    web_path = output_path('treemap.html', dataset='common', category='web')
+    treemap_job.command(
+        f'storage_visualization/treemap.py --output {web_path} --group-by-dataset {" ".join(f"--input {path}" for path in job_output_paths.values())}'
+    )
+
+    # Send a Slack message when the HTML page has been generated.
+    slack_job = batch.new_python_job(name='slack')
+    copy_common_env(slack_job)
+    slack_job.depends_on(treemap_job)
+    slack_job.call(
+        send_message,
+        'New storage visualization: ' + web_url('treemap.html', dataset='common'),
+    )
+
+    batch.run(wait=False)
+
+
+if __name__ == '__main__':
+    main()
