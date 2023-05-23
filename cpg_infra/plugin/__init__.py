34dd3c7202ca82f6f2729ef6dc7a9dfdcb029ad8
Michael Franklin <michael.franklin@populationgenomics.org.au>
2023-05-23 17:32:48 +1000

    PR cleanup

--- cpg_infra/plugin/__init__.py
+++ cpg_infra/plugin/__init__.py
@@ -1,11 +1,7 @@
 from cpg_infra.config import CPGInfrastructureConfig
 
-# from inspect import isclass, getmro
-from pathlib import Path
-# import importlib.util
-
-cpg_infra_path = str(Path(__file__).parent.parent.parent.absolute())
 PLUGIN_ENTRYPOINT_NAME = 'cpginfra.plugins'
+
 
 class CpgInfrastructurePlugin:
     """Billing aggregator Infrastructure (as code) for Pulumi"""
@@ -31,32 +27,3 @@ def get_plugins() -> dict:
         plugins[entry_point.name] = entry_point.load()
 
     return plugins
-
-
-    # expand path to include root of cpg-infrastructure directory so we can
-    sys.path.append(cpg_infra_path)
-
-    from metamist.infrastructure.driver import MetamistInfrastructure
-    from cpg_infra.billing_aggregator.driver import BillingAggregator
-
-    modules = [BillingAggregator, MetamistInfrastructure]
-
-    return modules
-
-    # root = Path(__file__).parent.parent
-    # known_paths = {
-    #     'metamist_infrastructure': str(root / 'metamist/infrastructure/driver.py'),
-    # }
-    # for key, module_path in known_paths.items():
-    #     spec = importlib.util.spec_from_file_location(key, module_path)
-    #     foo = importlib.util.module_from_spec(spec)
-    #
-    #     spec.loader.exec_module(foo)
-    #
-    #     for item in foo.__dict__.values():
-    #         if isclass(item) and item != CpgInfrastructurePlugin:
-    #             mro = getmro(item)
-    #             if 'CpgInfrastructurePlugin' in [cls.__name__ for cls in mro[1:]]:
-    #                 modules.append(item)
-    #
-    # return modules
