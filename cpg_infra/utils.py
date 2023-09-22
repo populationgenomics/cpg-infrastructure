ff0e43a70519b8eb872a43f13cce798449217e65
Milo Hyben <milo.hyben@gmail.com>
2023-09-22 11:37:28 +1000

    Extended archive_folder to allow customised and binary files.

--- cpg_infra/utils.py
+++ cpg_infra/utils.py
@@ -1,6 +1,7 @@
 """Utility methods that may be useful for multiple projects"""
-import os
 import contextlib
+import os
+from typing import Any, Callable
 
 import pulumi
 
@@ -8,9 +9,27 @@ DEFAULT_ALLOWED_EXTENSIONS = frozenset({'.py', '.txt', '.json'})
 
 
 def archive_folder(
-    path: str, allowed_extensions: frozenset[str] = DEFAULT_ALLOWED_EXTENSIONS
+    path: str,
+    allowed_extensions: frozenset[str] = DEFAULT_ALLOWED_EXTENSIONS,
+    apply_fun_map: dict[str, tuple[Callable, Any]] | None = None,
 ) -> pulumi.AssetArchive:
-    """Archive a folder into a pulumi asset archive"""
+    """Archive a folder into a pulumi asset archive
+
+    apply_fun_map contains map of file name to callable function and its extra parameter
+
+    e.g. apply_fun_map = {
+        'metamist-6.2.0.tar.gz': (add_binary_file, None),
+        'requirements.tx't: (update_requirements, {
+            'private_repo_url': 'https://...',
+            'private_repos': ['repo1','repo2'],
+        }),
+    }
+
+    where e.g. add_binary_file is defined as:
+    def add_binary_file(**kwargs):
+        return pulumi.FileAsset(f"{kwargs.get('path')}/{kwargs.get('filename')}")
+
+    """
     assets = {}
 
     # python 3.11 thing, but allows you to temporarily change directory
@@ -20,6 +39,19 @@ def archive_folder(
         for filename in os.listdir('.'):
             if not any(filename.endswith(ext) for ext in allowed_extensions):
                 # print(f'Skipping {filename} for invalid extension')
+                continue
+
+            # apply function to update file content
+            if apply_fun_map and filename in apply_fun_map:
+                (fun_name, extra_fun_params) = apply_fun_map[filename]
+                fun_params = {'filename': filename, 'path': path}
+                if extra_fun_params:
+                    # if extra params provided, then update fun_params
+                    fun_params.update(extra_fun_params)
+
+                # call the function to prepare the file asset
+                file_as_asset = fun_name(**fun_params)
+                assets[filename] = file_as_asset
                 continue
 
             with open(filename, encoding='utf-8') as file:
