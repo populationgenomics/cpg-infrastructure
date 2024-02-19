95357a0d78bbf0ec73ed04fdd05bd645583e76a9
Dan Coates <mail@dancoat.es>
2024-02-19 18:20:23 +1100

    add scaffold to allow testing bits of infra directly in cpg-infra

---
+++ test_stack/group_membership/__main__.py
@@ -0,0 +1,20 @@
+import pulumi
+
+from cpg_infra.abstraction.google_group_membership import (
+    GoogleGroupMembership,
+    GoogleGroupMembershipInputs,
+)
+
+
+def from_pulumi():
+    GoogleGroupMembership(
+        'test-group-membership',
+        props=GoogleGroupMembershipInputs(
+            group_id='abc',
+            member_key='foo@bar.com',
+        ),
+    )
+
+
+if __name__ == '__main__':
+    from_pulumi()
