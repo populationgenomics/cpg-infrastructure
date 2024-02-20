4cc823960e5d5854ff610a3c52bd2f1f6ac9b626
Michael Franklin <illusional@users.noreply.github.com>
2024-02-20 13:29:53 +1100

    Refactor GCP imports and remove unused code

--- scripts/remove_group_memberships.py
+++ scripts/remove_group_memberships.py
@@ -5,29 +5,38 @@ download the state file and remove the necessary bits
 """
 
 import json
+import os
 
 from google.cloud import storage
 
 
 def update_json():
-    storage_client = storage.Client()
-    source_bucket = storage_client.bucket("cpg-pulumi-state")
-    source_blob = source_bucket.blob(".pulumi/stacks/production.json")
-    data_str = source_blob.download_as_text()
+    if os.path.exists('production-old.json'):
+        with open('production-old.json', encoding='utf-8') as f:
+            data_str = f.read()
+    else:
+        storage_client = storage.Client()
+        source_bucket = storage_client.bucket("cpg-pulumi-state")
+        source_blob = source_bucket.blob(".pulumi/stacks/production.json")
+        data_str = source_blob.download_as_text()
 
-    with open('production-old.json', 'a') as f:
-        f.write(data_str)
+        with open('production-old.json', 'w+', encoding='utf-8') as f:
+            f.write(data_str)
 
     data = json.loads(data_str)
+    len_before = len(data['checkpoint']['latest']['resources'])
     data['checkpoint']['latest']['resources'] = [
         r
         for r in data['checkpoint']['latest']['resources']
         if r['type'] != 'gcp:cloudidentity/groupMembership:GroupMembership'
     ]
 
+    len_after = len(data['checkpoint']['latest']['resources'])
+    print(f"Removed {len_before - len_after} group memberships")
+
     result = json.dumps(data, indent=4)
 
-    with open('production.json', 'a') as f:
+    with open('production.json', 'w+', encoding='utf-8') as f:
         f.write(result)
 
 
