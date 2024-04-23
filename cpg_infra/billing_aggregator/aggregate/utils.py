a83832bb5082cf5be1b0cfb6aafabc87eae2a442
Milo Hyben <milo.hyben@gmail.com>
2024-04-24 09:14:34 +1000

    Renaming MAX_JOBS_PER_BATCH constants.

--- cpg_infra/billing_aggregator/aggregate/utils.py
+++ cpg_infra/billing_aggregator/aggregate/utils.py
@@ -94,10 +94,10 @@ ANALYSIS_RUNNER_PROJECT_ID = 'analysis-runner'
 
 # Maximum job count before all jobs gets summarised into one row
 # This is to prevent the bigquery inserts taking too long
-# Seqr jobs over 9K are going to be aggregated into one job.
-SEQR_MAX_JOBS_PER_BATCH = 9000
-# For hail jobs this limit is 50K
-HAIL_MAX_JOBS_PER_BATCH = 50000
+# Seqr / hail query jobs over 9K are going to be aggregated into one job.
+HAIL_QUERY_JOB_PER_BATCH_LIMIT = 9000
+# For hail non query jobs this limit is 50K
+HAIL_NON_QUERY_JOB_PER_BATCH_LIMIT = 50000
 
 # runs every 4 hours
 DEFAULT_RANGE_INTERVAL = timedelta(hours=int(os.getenv('DEFAULT_INTERVAL_HOURS', '4')))
@@ -672,9 +672,9 @@ async def process_entries_from_hail_in_chunks(
             return
 
         batch_name = batch.get('attributes', {}).get('name')
-        # check if hail batch query and too many jobs
-        if (batch_name is None and jobs_cnt > SEQR_MAX_JOBS_PER_BATCH) or (
-            batch_name is not None and jobs_cnt > HAIL_MAX_JOBS_PER_BATCH
+        # check if hail batch has too many jobs
+        if (batch_name is None and jobs_cnt > HAIL_QUERY_JOB_PER_BATCH_LIMIT) or (
+            batch_name is not None and jobs_cnt > HAIL_NON_QUERY_JOB_PER_BATCH_LIMIT
         ):
             # This is batch with too many jobs, we need to aggregate them
             # batch contains all the costs as cost_breakdown
