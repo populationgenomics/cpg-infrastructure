6e1a1a0df259291e2fd03b55925f11237648f898
Michael Franklin <illusional@users.noreply.github.com>
2024-05-13 15:05:28 +1000

    Make storage-viz more durable

--- storage_visualization/main.py
+++ storage_visualization/main.py
@@ -4,8 +4,6 @@
 
 import sys
 
-from cloudpathlib import AnyPath
-
 # See requirements.txt for why we're disabling the linter warnings here.
 import hailtop.batch as hb  # pylint: disable=import-error
 
@@ -20,14 +18,13 @@ from cpg_utils.hail_batch import (
     get_batch,
     prepare_git_job,
 )
-from cpg_utils.slack import upload_file
 
 DOCKER_IMAGE = (
     'australia-southeast1-docker.pkg.dev/cpg-common/images/storage-visualization:latest'
 )
 
 
-def prepare_job(job: hb.batch.job.Job, clone_repo: bool):
+def prepare_job(job: hb.batch.job.BashJob, clone_repo: bool):
     """Sets up the given job to run scripts in the same repository."""
     job.image(DOCKER_IMAGE)
     copy_common_env(job)
@@ -38,20 +35,6 @@ def prepare_job(job: hb.batch.job.Job, clone_repo: bool):
             repo_name=get_repo_name_from_current_directory(),
             commit=get_git_commit_ref_of_current_repository(),
         )
-
-
-def post_to_slack():
-    """Posts the URL of the generated treemap together with a preview image to Slack."""
-    with AnyPath(output_path('treemap.png', dataset='common', category='web')).open(
-        'rb',
-    ) as f:
-        content = f.read()
-
-    upload_file(
-        content=content,
-        comment='Storage visualization: '
-        + output_path('treemap.html', dataset='common', category='web_url'),
-    )
 
 
 def main():
@@ -72,6 +55,7 @@ def main():
         # getting preempted.
         job._preemptible = False  # pylint: disable=protected-access
         job.cpu(0.5)
+        job.memory('highmem')
 
         path = output_path(f'{dataset}.json.gz', dataset='common', category='analysis')
         job.command(f'storage_visualization/disk_usage.py {dataset} {path}')
@@ -81,19 +65,21 @@ def main():
     # Process the combined output of all jobs to generate a web report.
     treemap_job = batch.new_job(name='treemap')
     prepare_job(treemap_job, clone_repo=True)
+    # just don't show the specific report if it fails
+    treemap_job.always_run(True)
     for job in job_output_paths:
         treemap_job.depends_on(job)
 
-    web_path = output_path('treemap', dataset='common', category='web')
-    treemap_job.command(
-        f'storage_visualization/treemap.py --output-prefix {web_path} --group-by-dataset {" ".join(f"--input {path}" for path in job_output_paths.values())}',
+    input_commands = " ".join(
+        f"\\\n    --input {path}" for path in job_output_paths.values()
     )
-
-    # Send a Slack message when the HTML page has been generated.
-    slack_job = batch.new_python_job(name='slack')
-    prepare_job(slack_job, clone_repo=False)
-    slack_job.depends_on(treemap_job)
-    slack_job.call(post_to_slack)
+    treemap_job.command(
+        f"""
+storage_visualization/treemap.py \\
+    --group-by-dataset \\
+    --post-slack-message {input_commands}
+    """,
+    )
 
     batch.run(wait=False)
 
