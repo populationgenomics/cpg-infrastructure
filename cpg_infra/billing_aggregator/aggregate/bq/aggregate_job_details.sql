042441de3238f9e4b254917529d0db94b11e92a9
Milo Hyben <milo-hyben@users.noreply.github.com>
2025-09-04 22:46:08 +1000

    Update to Billing Materialised Views to reflect changes to Seq group and Cohorts (#315)
    * Update to Billing Materialised Views to reflect changes to Seq groups and Cohorts.
    
    * Billing BQ replacing JSON_EXTRACT_STRING_ARRAY with JSON_VALUE_ARRAY

--- cpg_infra/billing_aggregator/aggregate/bq/aggregate_job_details.sql
+++ cpg_infra/billing_aggregator/aggregate/bq/aggregate_job_details.sql
@@ -3,9 +3,12 @@ AS
 SELECT
     JSON_VALUE(PARSE_JSON(labels, wide_number_mode=>'round'), '$.ar-guid') as ar_guid,
     JSON_VALUE(PARSE_JSON(labels, wide_number_mode=>'round'), '$.batch_id') as batch_id,
-    COALESCE(JSON_VALUE(PARSE_JSON(labels, wide_number_mode=>'round'), '$.sequencing_group'), JSON_VALUE(PARSE_JSON(labels, wide_number_mode=>'round'), '$.sequencing-group')) as sequencing_group,
+    CASE WHEN JSON_QUERY(labels, '$.sequencing_groups') IS NOT NULL THEN ARRAY_TO_STRING(JSON_VALUE_ARRAY(labels, '$.sequencing_groups'), ',')
+    ELSE COALESCE(JSON_VALUE(PARSE_JSON(labels, wide_number_mode=>'round'), '$.sequencing_group'), JSON_VALUE(PARSE_JSON(labels, wide_number_mode=>'round'), '$.sequencing-group'))
+    END as sequencing_group,
+    CASE WHEN JSON_QUERY(labels, '$.cohorts') IS NOT NULL THEN ARRAY_TO_STRING(JSON_VALUE_ARRAY(labels, '$.cohorts'), ',') ELSE NULL END as cohorts,
     MIN(DATE_TRUNC(usage_start_time, DAY)) as min_day,
     MAX(DATE_TRUNC(usage_end_time, DAY)) as max_day
     FROM `billing-admin-290403.billing_aggregate.aggregate`
     WHERE cost_type <> 'tax'
-    GROUP BY ar_guid, batch_id, sequencing_group
+    GROUP BY ar_guid, batch_id, sequencing_group, cohorts
