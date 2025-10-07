a07ac5fec2f29ec952c097497a555a2c6b9f13f4
Milo Hyben <milo.hyben@gmail.com>
2025-10-07 14:52:33 +1100

    Add invoice month to billing.aggregate_job_details view.

--- cpg_infra/billing_aggregator/aggregate/bq/aggregate_job_details.sql
+++ cpg_infra/billing_aggregator/aggregate/bq/aggregate_job_details.sql
@@ -3,12 +3,24 @@ AS
 SELECT
     JSON_VALUE(PARSE_JSON(labels, wide_number_mode=>'round'), '$.ar-guid') as ar_guid,
     JSON_VALUE(PARSE_JSON(labels, wide_number_mode=>'round'), '$.batch_id') as batch_id,
-    CASE WHEN JSON_QUERY(labels, '$.sequencing_groups') IS NOT NULL THEN REGEXP_REPLACE(REPLACE(JSON_VALUE(PARSE_JSON(labels), '$.sequencing_groups'),"'",""), "\\[|\\]", "")
-    ELSE COALESCE(JSON_VALUE(PARSE_JSON(labels, wide_number_mode=>'round'), '$.sequencing_group'), JSON_VALUE(PARSE_JSON(labels, wide_number_mode=>'round'), '$.sequencing-group'))
+    CASE WHEN JSON_QUERY(labels, '$.sequencing_groups') IS NOT NULL
+    THEN 
+        UPPER(REGEXP_REPLACE(REPLACE(JSON_VALUE(PARSE_JSON(labels), '$.sequencing_groups'),"'",""), "\\[|\\]|[|]|\u0027,| |\"", ""))
+    ELSE 
+        UPPER(COALESCE(
+            JSON_VALUE(PARSE_JSON(labels, wide_number_mode=>'round'), '$.sequencing_group'),
+            JSON_VALUE(PARSE_JSON(labels, wide_number_mode=>'round'), '$.sequencing-group')
+        ))
     END as sequencing_group,
-    CASE WHEN JSON_QUERY(labels, '$.cohorts') IS NOT NULL THEN ARRAY_TO_STRING(JSON_VALUE_ARRAY(labels, '$.cohorts'), ',') ELSE NULL END as cohorts,
+    CASE WHEN JSON_QUERY(labels, '$.cohorts') IS NOT NULL
+    THEN 
+        ARRAY_TO_STRING(JSON_VALUE_ARRAY(labels, '$.cohorts'), ',')
+    ELSE 
+        NULL
+    END as cohorts,
+    invoice.month as invoice_month,
     MIN(DATE_TRUNC(usage_start_time, DAY)) as min_day,
     MAX(DATE_TRUNC(usage_end_time, DAY)) as max_day
     FROM `billing-admin-290403.billing_aggregate.aggregate`
     WHERE cost_type <> 'tax'
-    GROUP BY ar_guid, batch_id, sequencing_group, cohorts
+    GROUP BY ar_guid, batch_id, sequencing_group, cohorts, invoice_month
