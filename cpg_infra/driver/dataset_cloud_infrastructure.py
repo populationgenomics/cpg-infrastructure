bc45e7ff7b71b56911be047fbaf5181f00323dc1
Milo Hyben <milo.hyben@gmail.com>
2026-09-15 15:47:49 +1000

    Remove dead scaffolding.

--- cpg_infra/driver/dataset_cloud_infrastructure.py
+++ cpg_infra/driver/dataset_cloud_infrastructure.py
@@ -709,15 +709,6 @@ class CPGDatasetCloudInfrastructure:
                 f'Could not find infra to save blob to for config_destination: '
                 f'{self.config.config_destination}',
             )
-        # DryRunInfra returned above and the regex check just rejected any
-        # non-`gs://` destination, so at this point self.infra is always
-        # GcpInfrastructure. The assert is a static-type nudge for the
-        # `_infra_to_call_function_on = self.infra` narrowing below -- it
-        # would only ever fire if a future backend chose the `gs://` scheme
-        # and slipped past the regex, which isn't a realistic scenario.
-        assert isinstance(self.infra, GcpInfrastructure)
-        _infra_to_call_function_on = self.infra
-
         bucket_name, suffix = self.config.config_destination.removeprefix(
             'gs://',
         ).split('/', maxsplit=1)
@@ -729,7 +720,7 @@ class CPGDatasetCloudInfrastructure:
             f'{self.infra.name()}/{self.dataset_config.dataset}-{namespace}' + '.toml',
         )
 
-        _infra_to_call_function_on.add_blob_to_bucket(
+        self.infra.add_blob_to_bucket(
             resource_name=f'storage-config-{name}',
             bucket=bucket_name,
             output_name=output_name,
