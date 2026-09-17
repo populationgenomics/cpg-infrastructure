ae89ebc22d7a5e84dd0f5deb41fd9fac4b1e0d01
Milo Hyben <milo.hyben@gmail.com>
2026-09-17 14:44:16 +1000

    Add new custom role seqeraAutoscalingPolicyUser to allow seqera to use Dataproc autoscaling policies.

--- cpg_infra/driver/dataset_seqera_infrastructure.py
+++ cpg_infra/driver/dataset_seqera_infrastructure.py
@@ -271,12 +271,18 @@ class DatasetSeqeraInfrastructure:
         # autoscaling policies defined centrally in the common project
         # (projects/cpg-common/regions/*/autoscalingPolicies/*) when Nextflow
         # tasks spin up Dataproc clusters that reference those policies.
+        # The predefined roles/dataproc.autoscalingPolicyUser role cannot be
+        # bound at project scope, so we bind an equivalent custom role
+        # defined once on cpg-common.
         common_project_id = self._parent.root.common_gcp_infra.project_id
+        autoscaling_role = (
+            self._parent.root.common_seqera_autoscaling_policy_user_role
+        )
         for level, sa in self._service_accounts.items():
             self._infra.add_project_role(
                 f'seqera-{level}-common-dataproc-autoscalingpolicyuser',
                 member=sa,
-                role='roles/dataproc.autoscalingPolicyUser',
+                role=autoscaling_role.name,
                 project=common_project_id,
             )
 
